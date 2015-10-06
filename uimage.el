34cb09448593587f40fcb057cb8144ebeda65463
darksun <lujun9972@gmail.com>
2015-10-06 08:18:56 +0800

    更改说明

--- uimage.el
+++ uimage.el
@@ -3,8 +3,27 @@
 ;; Copyright (C) 2004-2015 Free Software Foundation, Inc.
 
 ;; Author: DarkSun <lujun9972@gmail.com>
-;; Keywords: multimedia
+;; Keywords: lisp, url, image
 
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Source code
+;;
+;; uimage's code can be found here:
+;;   http://github.com/lujun9972/uimage
 ;;; Commentary:
 
 ;; Uimage is a iimange like minor mode but could displays url images
